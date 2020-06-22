d07bc52aaa1e76ec0e6bf09254f7e3b0156df831
Joypurohit <63277296+Joypurohit@users.noreply.github.com>
2020-06-22 16:58:18 +0530

    Update homePage.js

--- homePage.js
+++ homePage.js
@@ -415,7 +415,8 @@ else{
         });
 
     });
-    for(var i=1;i<array.length;i++)
+}
+             for(var i=1;i<array.length;i++)
     {
         for(var j=0;j<n-i;j++)
         {
@@ -429,9 +430,6 @@ else{
         }
 
     }
-    
-
-}
             res.render('home',{data:array});
             
              
